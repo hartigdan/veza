a379e4433326e44ac7582875f1528f2bab30487e
Daniel Hartig <daniel@mindekar.net>
2021-09-14 21:02:07 +0200

    Add preset-env for babel-loader

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -24,9 +24,12 @@ module.exports = {
         }, {
             test: /\.js$/,
             exclude: /node_modules/,
-            use: [
-                'babel-loader'
-            ]
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
         }, {
             test: /\.(ijmap|svg|ttf|woff|woff2|eot)$/i,
             loader: 'file-loader'
