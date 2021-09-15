3685c921bcac489b7f73afe4d025c6d88434427a
felix.monninger@esrlabs.com <felix.monninger@partner.bmw.com>
2021-09-15 16:01:47 +0200

    Adapt tx_packets.sh to new rust workspace

--- probe/pipe/test/tx_packets.sh
+++ probe/pipe/test/tx_packets.sh
@@ -1,1 +1,1 @@
-echo "cat /sys/class/net/lo/statistics/tx_packets" | RUST_LOG=debug ./target/debug/pipe --id tx_packets
+echo "cat /sys/class/net/lo/statistics/tx_packets" | RUST_LOG=debug ../../target/debug/pipe --id tx_packets
