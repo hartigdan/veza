14f29a5bd300a5cc55ec574f7b5bf5f202fd4a65
Dietrich Spaeth <fndrd1d1@mailbox.org>
2021-09-15 16:37:02 +0200

    Filter syntax

--- probe/can/src/main.rs
+++ probe/can/src/main.rs
@@ -8,6 +8,9 @@ struct Cli {
     #[structopt(long)]
     id: String,
 
+    #[structopt(short, long)]
+    count: Option<u32>,
+
     interface: String,
 
     #[structopt(help = "filter syntax: id:byte:bit")]
@@ -16,9 +19,12 @@ struct Cli {
 
 fn main() -> Result<()>{
     let args = Cli::from_args();
+
     let socket = CANSocket::open(args.interface.as_str())?;
     let filter_tuple: Vec<&str> = args.filter.split(':').collect();
-    loop
+
+    let mut count = args.count.unwrap_or(1);
+    while count > 0 || args.count == None
     {
         let packet = socket.read_frame()?;
         if packet.id() == filter_tuple[0].parse::<u32>()?
@@ -34,10 +40,18 @@ fn main() -> Result<()>{
                     let bit = filter_tuple[2].parse::<u32>()?;
                     value = (value >> bit) & 1;
                 }
-
                 let msg = json!({"id": args.id, "value": value});
                 println!("{}", msg);
             }
+            else
+            {
+                let msg = json!({"id": args.id, "value": packet.data()});
+                println!("{}", msg);
+            }
+        }
+        if args.count != None
+        {
+            count -= 1;
         }
     }
     Ok(())
