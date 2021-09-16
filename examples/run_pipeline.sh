a057c1b12ebed64c8b0592582dc108110536e01e
felixmonninger-esr <86418253+felixmonninger-esr@users.noreply.github.com>
2021-09-16 12:20:51 +0200

    rename merge to mux (#16)
    Co-authored-by: felix.monninger@esrlabs.com <felix.monninger@partner.bmw.com>

--- examples/run_pipeline.sh
+++ examples/run_pipeline.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 _term() {
-    kill ${pid_merge}
+    kill ${pid_mux}
     kill ${pid_serve}
     kill ${pid_server}
 }
@@ -10,8 +10,8 @@ trap _term INT TERM
 
 ../target/debug/serve &
 pid_serve=$!
-../target/debug/merge --bind 127.0.0.1:8888 127.0.0.1:8081 &
-pid_merge=$!
+../target/debug/mux --bind 127.0.0.1:8888 127.0.0.1:8081 &
+pid_mux=$!
 sleep 1;
 socat exec:"python ../serve/dummy_input.py" tcp:127.0.0.1:8888 &
 make -C ../ui/ serve-files &
